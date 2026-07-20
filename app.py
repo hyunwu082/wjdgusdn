85576f871d495ae0509490a3d49f9efb74699390
hyunwu082 <25s10528@guro.sen.hs.kr>
2026-07-20 11:19:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ st.markdown("# 앱 UI 만들기")
 user_id = st.text_input("이름", placeholder="이름")
 st.radio("학년", ["1", "2", "3"], horizontal=True)
 st.number_input("반", min_value=1, max_value=11, value= 반)
-st.select_slider("응답 처리 속도를 선택하세요",options=["매우 느림", "느림", "보통", "빠름", "실시간"],value="보통")
+난이도= st.select_slider("난이도",options=["매우 느림", "느림", "보통", "빠름", "실시간"],value="보통")
+점수= st.slider("점수", 0, 100, 50)
+
