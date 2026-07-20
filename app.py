2fef78af3e3667a7e15b16c720333b53988b72f9
hyunwu082 <25s10528@guro.sen.hs.kr>
2026-07-20 11:20:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.radio("학년", ["1", "2", "3"], horizontal=True)
 st.number_input("반", min_value=1, max_value=11, value= 반)
 난이도= st.select_slider("난이도",options=["매우 느림", "느림", "보통", "빠름", "실시간"],value="보통")
 점수= st.slider("점수", 0, 100, 50)
-
+question = st.text_area("소감", placeholder="소감입니다.")
