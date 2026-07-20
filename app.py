f40307c67ab72f279cbf2116d6c10d7b38a2ca13
hyunwu082 <25s10528@guro.sen.hs.kr>
2026-07-20 11:24:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.markdown("# 앱 UI 만들기")
 user_id = st.text_input("이름", placeholder="이름")
 학년= st.radio("학년", ["1", "2", "3"], horizontal=True)
-반= st.number_input("반", min_value=1, max_value=11, value= 반)
+반= st.number_input("반", min_value=1, max_value=11, value= '반')
 난이도= st.select_slider("난이도",options=["매우 느림", "느림", "보통", "빠름", "실시간"],value="보통")
 점수= st.slider("점수", 0, 100, 50)
 question = st.text_area("소감", placeholder="소감입니다.")
