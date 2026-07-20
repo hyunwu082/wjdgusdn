425873fb023ff59d8ac6ec5a64783c4cd6cd0215
hyunwu082 <25s10528@guro.sen.hs.kr>
2026-07-20 11:12:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.markdown("# 앱 UI 만들기")
 user_id = st.text_input("이름", placeholder="이름")
-st.radio("학년, ["1", "2", "3"], horizontal=true")
+st.radio("학년", ["1", "2", "3"], horizontal=true")
