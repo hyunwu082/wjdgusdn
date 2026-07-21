d29da3f6671500ca0ac9cfdc0f622d80025a7ad5
hyunwu082 <25s10528@guro.sen.hs.kr>
2026-07-21 13:26:50 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -20,15 +20,15 @@ def add_todo():
 @st.dialog("개념 수정 ")
 def edit_motto():
     motto = st.text_input("내가 알고 싶은 개념을 적어주세요.")
-    if st.button("다짐 저장"):
+    if st.button("개념 저장"):
         st.session_state.user_motto = motto
         st.session_state.motto_updated = True
         st.rerun()
 
 def page_motto():
-    st.header("📣 1. 오늘의 다짐")
-    st.info(f"현재 다짐: {st.session_state.user_motto}")
-    if st.button("다짐 수정하기"):
+    st.header("📣 1. 오늘의 개념")
+    st.info(f"개념: {st.session_state.user_motto}")
+    if st.button("개념 수정하기"):
         edit_motto()
     if st.session_state.motto_updated:
         st.success("새로운 좌우명이 등록되었습니다!")
